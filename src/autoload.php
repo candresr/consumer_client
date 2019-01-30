1d01d7ab0419447ddbbde3642cdd177fc11a27cf
cesar <caramirez@asesoftware.com>
2019-01-30 10:55:49 -0500

    ajustando

--- src/autoload.php
+++ src/autoload.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once  __DIR__.'/../vendor/autoload.php';
+//require_once  __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/Auth.php';
 require_once __DIR__.'/Util/AlmString.php';
 require_once __DIR__.'/Util/AlmArray.php';
