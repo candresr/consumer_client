2f051b2f32d0e7988bf49b6d431eaa7dc28c057f
cesar <caramirez@asesoftware.com>
2019-01-30 11:16:57 -0500

    main

--- src/autoload.php
+++ src/autoload.php
@@ -6,7 +6,7 @@ require_once __DIR__.'/Util/AlmString.php';
 require_once __DIR__.'/Util/AlmArray.php';
 require_once __DIR__.'/Util/AlmDate.php';
 require_once __DIR__.'/Util/AlmValidator.php';
+require_once __DIR__ . '/Main.php';
 require_once __DIR__.'/Resource/Terceros.php';
 require_once __DIR__.'/Resource/Siab.php';
 require_once __DIR__.'/Resource/Construdata.php';
-require_once __DIR__.'/main.php';
